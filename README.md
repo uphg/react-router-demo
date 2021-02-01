9057725eee9f5f6eb776c1b575f28fc3febbf2c2
lvheng <1834805770@qq.com>
2021-02-01 22:03:19 +0800

    使用 React Router

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - [x] 实现一个简单的 React Hash 模式路由
 - [x] 实现一个简单的 React History 模式路由
+- [x] 使用 React Router
--- src/App.js
+++ src/App.js
@@ -1,51 +1,51 @@
-import React, { useState } from "react";
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+
 import './App.css';
 
-function About () {
-  return(
+function About() {
+  return (
     <div>我是关于页</div>
   )
 }
 
-function User () {
-  return(
+function User() {
+  return (
     <div>我是用户页</div>
   )
 }
 
-function Home () {
-  return(
+function Home() {
+  return (
     <div>我是首页</div>
   )
 }
 
 function App() {
-  const [history, setHistory] = useState(window.location.pathname)
-  const clickLink = (link) => {
-    setHistory(link)
-    window.history.pushState(null, '', link)
-  }
-
-  const loadingPage = (path) => {
-    if (path === '/') {
-      return <Home />
-    } else if (path === '/user') {
-      return <User />
-    } else if (path === '/about') {
-      return <About />
-    }
-  }
   return (
-    <div className="App">
-      <header>
-        <button onClick={() => { clickLink('/') }}>首页</button>
-        <button onClick={() => { clickLink('/user') }}>用户</button>
-        <button onClick={() => { clickLink('/about') }}>关于</button>
-      </header>
-      <main>
-        {loadingPage(history)}
-      </main>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> | <Link to="/user">User</Link>
+        </nav>
+        <Switch>
+          <Route path="/about">
+            <About />
+          </Route>
+          <Route path="/user">
+            <User />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
